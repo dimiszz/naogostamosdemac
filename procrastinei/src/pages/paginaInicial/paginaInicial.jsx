569a6db5440355234af236307e9c60e124c8f025
Jolid Pnake <66273650+JolidPnake@users.noreply.github.com>
2023-06-18 13:43:57 -0300

    PaginaInicial

--- procrastinei/src/pages/paginaInicial/paginaInicial.jsx
+++ procrastinei/src/pages/paginaInicial/paginaInicial.jsx
@@ -7,40 +7,17 @@ export default function paginaInicial() {
         <div class="container-login100">
           <div class="wrap-login100">
 
-            <form class="login100-form validate-form">
-              <span class="login100-form-title">
-                Cadastrar-se
-              </span>
-
-              <div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-                <input class="input100" type="text" name="email" placeholder="Email" />
-                <span class="focus-input100"></span>
-                <span class="symbol-input100">
-                  <i class="fa fa-envelope" aria-hidden="true"></i>
-                </span>
-              </div>
-
-              <div class="wrap-input100 validate-input" data-validate = "Password is required">
-                <input class="input100" type="password" name="pass" placeholder="Password" />
-                <span class="focus-input100"></span>
-                <span class="symbol-input100">
-                  <i class="fa fa-lock" aria-hidden="true"></i>
-                </span>
-              </div>
+            <div class="login100-pic js-tilt" data-tilt>
+              <img src="images/img-01.png" alt="IMG" />
+            </div>
               
               <div class="container-login100-form-btn">
                 <button class="login100-form-btn">
                   Login
                 </button>
-              </div>
-
-              <div class="text-center p-t-12">
-                <span class="txt1">
-                  Forgot
-                </span>
-                <a class="txt2" href="#">
-                  Username / Password?
-                </a>
+                <button class="login100-form-btn">
+                  Cadastrar-se
+                </button>
               </div>
 
               <div class="text-center p-t-136">
@@ -49,7 +26,6 @@ export default function paginaInicial() {
                   <i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
                 </a>
               </div>
-            </form>
           </div>
         </div>
       </div>
