d4807de2f51edeca9f0e0f683f3495b1f6987961
Jolid Pnake <66273650+JolidPnake@users.noreply.github.com>
2023-06-18 12:02:57 -0300

    testesite

---
+++ nlw/public/scripts/intro.js
@@ -0,0 +1,67 @@
+// comentarios
+// document.write("hello")
+
+// variaveis, tipos de dados
+// let myvar = "Hello"
+// const myconst = "he"
+
+// document.write(myconst + myvar)
+
+// //  string
+// const s1 = "String"
+// const s2 = 'String'
+// const s3 = `String`
+
+// // number
+// const n1 = "1"
+// const n2 = 12
+// document.write(n1 + n2)
+
+// // boolean - true or false
+// const bTrue = true
+// const bFalse = false
+// document.write(bTrue)
+
+// objeto possuem propriedades e funcionalidade
+// const pessoa = {
+//   altura: "1,80m",
+//   idade : 24,
+//   solteiro: true,
+//   correr (){
+//     document.write("run forest")
+//   }
+// }
+
+// pessoa.correr()
+
+// Arrey - vetores
+// const ColecaoDeBolinhas = ["blue", "green", 1, {name: "My Name"}]
+
+// document.write(ColecaoDeBolinhas[3].name)
+
+// Funções - Funcionalidades - Atalhos - Reuso de código
+// // registrar função
+// function sayMyName(name) {
+//   document.write(name)
+// }
+
+// // Execeutar
+// sayMyName("Douglas")
+// sayMyName("Valeska")
+// sayMyName("Kyam")
+
+//condicionais
+
+// const notaFinal = 7
+
+// if( notaFinal <5 ) {
+//   document.write("Reprovado")
+// } else {
+//   document.write("Aprovado")
+// }
+
+// loop repetições
+
+// for (i = 0; i < 10; i++) {
+//     document.write(`<p>${i}</p>`)
+//   }
