839d8af917c87737ff7093c2008c7b03ab02ae4e
Miguel03706 <migueltjsilva@gmail.com>
2023-06-18 13:28:03 -0300

    template

--- procrastinei/src/index.js
+++ procrastinei/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Rotas from './rotas.js'
+import './pages/login/main.css'
+import './pages/login/util.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- procrastinei/src/pages/login/login.jsx
+++ procrastinei/src/pages/login/login.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import './main.css'
-import './util.css'
 
 export default function login() {
   return (
@@ -17,8 +15,17 @@ export default function login() {
 						Cadastrar-se
 					</span>
 
+                    <div class="wrap-input100 validate-input" data-validate = "é necessário um email válido: ex@abc.xyz">
+                        <span className='m-1'> Nome: </span>
+						<input class="input100" type="text" name="email" placeholder="Nome de usuário" value="Estudante"/>
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-envelope" aria-hidden="true"></i>
+						</span>
+					</div>
+
 					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-						<input class="input100" type="text" name="email" placeholder="Email" />
+						<input class="input100" type="text" name="email" placeholder="Email" value="teste@gmail.com"/>
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
 							<i class="fa fa-envelope" aria-hidden="true"></i>
@@ -26,31 +33,44 @@ export default function login() {
 					</div>
 
 					<div class="wrap-input100 validate-input" data-validate = "Password is required">
-						<input class="input100" type="password" name="pass" placeholder="Password" />
+						<input class="input100" type="password" name="pass" placeholder="Password" value="1234"/>
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
 							<i class="fa fa-lock" aria-hidden="true"></i>
 						</span>
 					</div>
+                    <div class="wrap-input100 validate-input" data-validate = "Password is required">
+						<input class="input100" type="password" name="pass" placeholder="Password" value="1234"/>
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-lock" aria-hidden="true"></i>
+						</span>
+					</div>
+                    <div class="wrap-input100 validate-input" data-validate = "Password is required">
+						<input class="input100" type="password" name="pass" placeholder="Password" value="1234"/>
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-lock" aria-hidden="true"></i>
+						</span>
+					</div>
+                    
 					
 					<div class="container-login100-form-btn">
 						<button class="login100-form-btn">
-							Login
+							Entrar
 						</button>
 					</div>
 
 					<div class="text-center p-t-12">
-						<span class="txt1">
-							Forgot
-						</span>
-						<a class="txt2" href="#">
-							Username / Password?
+						<a class="txt2 p-t-136" href="#">
+							Recuperar senha
 						</a>
 					</div>
 
 					<div class="text-center p-t-136">
+                        <span>Não possui uma conta? </span>
 						<a class="txt2" href="#">
-							Create your Account
+                            Criar conta
 							<i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
 						</a>
 					</div>
