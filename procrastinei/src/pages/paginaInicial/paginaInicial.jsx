8c7df938a88959f65a1197b6f08d396ba4460bed
Jolid Pnake <66273650+JolidPnake@users.noreply.github.com>
2023-06-18 13:27:15 -0300

    Pagina Inicial

--- procrastinei/src/pages/paginaInicial/paginaInicial.jsx
+++ procrastinei/src/pages/paginaInicial/paginaInicial.jsx
@@ -3,7 +3,56 @@ import React from 'react'
 export default function paginaInicial() {
   return (
     <>
-     <div>paginaInicial</div>    
+     <div class="limiter">
+        <div class="container-login100">
+          <div class="wrap-login100">
+
+            <form class="login100-form validate-form">
+              <span class="login100-form-title">
+                Cadastrar-se
+              </span>
+
+              <div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+                <input class="input100" type="text" name="email" placeholder="Email" />
+                <span class="focus-input100"></span>
+                <span class="symbol-input100">
+                  <i class="fa fa-envelope" aria-hidden="true"></i>
+                </span>
+              </div>
+
+              <div class="wrap-input100 validate-input" data-validate = "Password is required">
+                <input class="input100" type="password" name="pass" placeholder="Password" />
+                <span class="focus-input100"></span>
+                <span class="symbol-input100">
+                  <i class="fa fa-lock" aria-hidden="true"></i>
+                </span>
+              </div>
+              
+              <div class="container-login100-form-btn">
+                <button class="login100-form-btn">
+                  Login
+                </button>
+              </div>
+
+              <div class="text-center p-t-12">
+                <span class="txt1">
+                  Forgot
+                </span>
+                <a class="txt2" href="#">
+                  Username / Password?
+                </a>
+              </div>
+
+              <div class="text-center p-t-136">
+                <a class="txt2" href="#">
+                  Create your Account
+                  <i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
+                </a>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </>
 
   )
